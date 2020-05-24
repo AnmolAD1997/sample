d783545b5ba9dbedad95f1e5dd45d456146eef39
Anmol Adhikari <adhikari.anmol97@gmail.com>
2020-05-24 15:59:07 -0500

    sample

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-let hello="Anmol Adhikari";
+let express=require('express');
 
-console.log("hello and welcome to AWS. "+hello)
+let app=express();
+
+
+app.use('/',(req,res)=>{
+
+    res.send("Server is here babies!!");
+
+})
+console.log("Server creation statred");
+
+app.listen(5000,()=>{
+
+
+console.log("server create at: 5000");
+
+});
